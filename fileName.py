cdd6e0085cfc7fdf8e870c58e2166a517863114d
tommyfgj <412816322@qq.com>
2016-04-27 01:09:18 +0800

    modified:   conf/config.ini 	modified:   conf/log.xml 	new file:   fileName.py 	modified:   install.sh

---
+++ fileName.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import sys
+
+str = sys.argv[1]
+new = str.replace('/', '\/')
+print new
--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+CUR=$(cd "$(dirname "$0")"; pwd)"/"
 GO=go
 SRC=./src/cctvbnews.go
 TASK=cctvbnews
@@ -7,3 +8,6 @@ TASK=cctvbnews
 ${GO} build ${SRC} 
 mv ${TASK} ./bin
 
+zCUR=`python fileName.py ${CUR}`
+sed -i "s/\${DIR}/${zCUR}/g" ./conf/config.ini
+sed -i "s/\${DIR}/${zCUR}/g" ./conf/log.xml
